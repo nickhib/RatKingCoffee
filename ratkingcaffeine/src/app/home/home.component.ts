bf7acca1248737ffe03914605bcbfb36e894df6f
nickhib <nwhibbard@mail.csuchico.edu>
2024-02-01 18:44:52 -0800

    added a slide show container
    the slide show container uses java script ,css and html.
    currently there is a 1/3 caption on top left side of the
    slide show container, which need to be removed.
    captions at the bottome middle needs to be removed
    can move slide show with arrows on the sides or dots on the bottom

--- ratkingcaffeine/src/app/home/home.component.ts
+++ ratkingcaffeine/src/app/home/home.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  slideIndex = 1;
 
+  @ViewChild('dot1') dot1!: ElementRef;
+  @ViewChild('dot2') dot2!: ElementRef;
+  @ViewChild('dot3') dot3!: ElementRef;
+
+  ngAfterViewInit() {
+    this.showSlides(this.slideIndex);
+  }
+
+  plusSlides(n: number) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  currentSlide(n: number) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n: number) {
+    let i;
+    const slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+    const dots = [this.dot1.nativeElement, this.dot2.nativeElement, this.dot3.nativeElement];
+
+    if (n > slides.length) { this.slideIndex = 1; }
+    if (n < 1) { this.slideIndex = slides.length; }
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[this.slideIndex - 1].style.display = "block";
+    dots[this.slideIndex - 1].className += " active";
+  }
 }
