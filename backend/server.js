15fb914e0462a3285fe4b1bb5f7daa61f124fda8
nickhib <98436821+nickhib@users.noreply.github.com>
2026-04-01 17:51:28 -0700

    25 stripe webhook (#36)
    * created endpoint for the webhook in the backend
    
    * added payment verification, cart clear to the stripe webhook
    
    * restrutured stripe webhook, added a table for orders
    
    * added new tables for payments and orders
    
    * creates order id on payment intent and sets up tables to take in orders and payments
    
    * fixed bug with backend not creating orders
    
    * working on updating orders
    
    * intial start to fixing testing
    
    * added orders, order-items, and payments to the backend
    
    * fixed verification error checking
    
    * returns orderid with payment intent.
    
    * added res.status(200) this makes for a better return. cart needs more work. need to set orderid in a cookie.
    
    * added cookie for order id, then did res.status to return.
    
    * began adding existing paymentIntent logic, edits payment intent depending on situation
    
    * fixed some butgs with the stripe service not returning
    
    * created the editpayment function, updated error checking, updated webhook
    
    * added instant response to the webhook so stripe doesnt retry
    
    * added error checking to backend webhook
    
    * fixed some bugs with backend. need to fix more
    
    * made seperate router/controller for webhook
    
    * fixed bugs with, webhook tested, removed res from business logic
    
    * fixed bugs with, webhook tested, removed res from business logic
    
    * installed bull using redis to create queues to follow best practices on stripe
    
    * added queue to webhook for scaling, order creation edits
    
    * added docker compose to run application
    
    * added nginx, tried to fix docker compose bugs
    
    * added nginx, added Docker files, Docker compose, can now start app with docker compose
    
    * fixed stripe bug, mismatched keys
    
    * added jobs queue for payments in the webhook
    
    * added console log to backend
    
    * got rid of unused import
    
    * changed order of server start up

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,10 @@ import { initDb } from './apps/component-a/data-access/initDatabase.js'
 const PORT = process.env.PORT || 3000;
 
 (async () => {
-  await initDb();
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server listening on http://localhost:${PORT}`);
-});})();
+});
+await initDb();
+})();
 
 // reference this: https://github.com/goldbergyoni/nodebestpractices
